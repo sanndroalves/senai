85a3d1203e634afeb2190f66645132e2687bbc55
Alessandro Alves <100237308+sanndroalves@users.noreply.github.com>
2023-05-11 22:36:05 -0300

    Delete employeesAtv.sql

--- employeesAtv.sql
+++
@@ -1,7 +0,0 @@
-
--- (a)
-select count(*) as TOTAL from (select distinct e.employeeNumber from customers c
-join employees e on e.employeeNumber = c.salesRepEmployeeNumber]
-order by e.employeeNumber) t;
-
--- (b)
